92db9391bb8e56c1cef40f14ebc5f0f0c265d8a3
Gilberto Almeida <almeidanetogilberto@gmail.com>
2020-05-29 01:51:09 +0200

    These files are also part of the previous commit about the creation of the controller package

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -7,15 +7,36 @@ import { Routes } from "./routes";
 import "./index.css";
 
 ReactDOM.render(
+  <React.Fragment>
+    <ApolloProvider
+      client={client}
+      // apolloprovider allows to do graphql requests from anywhere in the app
+    >
+      <Routes />
+    </ApolloProvider>
+  </React.Fragment>,
+  document.getElementById("root")
+);
+
+/* 
+I am not running strict mode bc I was getting always a console warning
+index.js:1 Warning: findDOMNode is deprecated in StrictMode.
+This is still to be fixed, so until then I am not running in strict
+bc the warning is annoying
+
+
+ReactDOM.render(
   <React.StrictMode>
-    {/* apolloprovider allow to do graphql requests from
-    anywhere in the app */}
-    <ApolloProvider client={client}>
+    <ApolloProvider
+      client={client}
+      // apolloprovider allows to do graphql requests from anywhere in the app
+    >
       <Routes />
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
+ */
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,16 +1,26 @@
 import React, { PureComponent } from "react";
+import { RegisterController } from "@cribswap/controller";
+
 import { RegisterView } from "./ui/RegisterView";
 
-export class RegisterConnector extends PureComponent {
-  dummySubmit = async (values: any) => {
-    console.log(values);
-    return null;
-  };
+/* So, We have a controller that does the calls to the backend.
+The controllers are in a separate package, which is imported to
+web and will be imported to the app too. This is for sharing code
+This file here is the connector, which is meant to connect the
+controller and pass it to a View with a function. */
 
+export class RegisterConnector extends PureComponent {
   render() {
     return (
       <div>
-        <RegisterView submit={this.dummySubmit} />
+        <RegisterController
+        /* the children of the register controller is this function
+        which pass submit to the view. In the controller package
+        we are passing the submit function inside an object to the
+        this.props.children. That's why we can get it here */
+        >
+          {({ submit }) => <RegisterView submit={submit} />}
+        </RegisterController>
       </div>
     );
   }
